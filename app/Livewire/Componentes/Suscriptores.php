ae888dc4d868f41349964e547e106478bbd7a486
Rene Daniel Merlo <rmerlo@gmail.com>
2025-10-29 10:20:25 -0300

    Sincronización al 29/10/2025

--- app/Livewire/Componentes/Suscriptores.php
+++ app/Livewire/Componentes/Suscriptores.php
@@ -2,10 +2,41 @@
 
 namespace App\Livewire\Componentes;
 
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class Suscriptores extends Component
 {
+    public $txtEmail = '';
+    public $mensajeConfirmacion = '';
+
+    public function GrabarMail(){
+        
+        // Eliminar espacios al inicio y al final (y en el medio, si querés)
+        $this->txtEmail = trim($this->txtEmail);
+
+        // También podés normalizar espacios intermedios:
+        $this->txtEmail = preg_replace('/\s+/', '', $this->txtEmail);        
+        
+        $this->validate([
+            'txtEmail' => ['required', 'email', 'unique:tb_suscriptores,email'],
+        ], [
+            'txtEmail.required' => 'Debe ingresar un email válido',
+            'txtEmail.email' => 'Email no válido',
+            'txtEmail.unique' => 'El email ya está registrado',
+        ]);
+
+        DB::table('tb_suscriptores')->insert([
+            'email' => $this->txtEmail,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        $this->txtEmail = '';
+        $this->mensajeConfirmacion = '¡Gracias por suscribirte!';
+        $this->dispatch('mensaje-enviado');        
+    }
+
     public function render()
     {
         return view('livewire.componentes.suscriptores');
--- resources/views/livewire/componentes/suscriptores.blade.php
+++ resources/views/livewire/componentes/suscriptores.blade.php
@@ -1,10 +1,37 @@
-<section class="bg-[#FEF0E9]  flex flex-col px-5 py-[5rem] md:px-20 md:py-[5rem]">
+<section class="bg-[#FEF0E9]  flex flex-col px-5 py-[5rem] md:px-20 md:py-[5rem]"
+    x-data="{ mostrarMensaje: false }"
+    x-init="@this.on('mensaje-enviado', () => { 
+        mostrarMensaje = true; 
+        setTimeout(() => mostrarMensaje = false, 5000); 
+    })">
     <p class="text-[#1F1F1F] text-[1.5rem] md:text-[2.5rem] ">Recibe en tu mail todas nuestras ofertas</p>
     <p class="text-[#1F1F1F] text-md mb-[1rem] ">Déjanos tu e-mail y te enviaremos todas nuestras ofertas, promos y nuevos productos</p>
-    <div class="w-[95%] md:w-[50%] flex justify-center rounded-md">
-        <input type="text" class="mr-[1rem] block p-2 w-full bg-[#FEF0E9] border-[#1F1F1F] border-[1px] rounded-md text-lg focus:outline-none focus:ring-0" placeholder="Ingresa tu email...">
-        <button class="p-2 text-white px-[2rem] cursor-pointer bg-[#5D7857] hover:bg-[#405D39]">
+    <div class="w-[95%] md:w-[50%] flex items-start rounded-md">
+        <div class="flex flex-col flex-1 mr-[1rem]"> 
+            <input wire:model="txtEmail" 
+                type="text" 
+                class="block p-2 w-full bg-[#FEF0E9] border-[#1F1F1F] 
+                    @error('txtEmail') border-red-500 @else border-[#1F1F1F] @enderror 
+                    border-[1px] rounded-md text-lg focus:outline-none focus:ring-0" 
+                    placeholder="Ingresa tu email...">
+    
+            @error('txtEmail')
+                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>                        
+            @enderror
+        </div>
+    
+        <button wire:click="GrabarMail()" 
+            class="self-start h-[45px] p-2 text-white px-[2rem] cursor-pointer bg-[#5D7857] hover:bg-[#405D39]">
             Enviar
         </button>
-    </div>        
+    </div>
+        <div 
+        x-show="mostrarMensaje"
+        x-transition
+        class="text-[#405D39] mt-3 text-lg font-semibold">
+        {{ $mensajeConfirmacion }}
+    </div>    
+
 </section>
+
+
